a585f7124fce522d98399b089ca263ff3fade23f
reeganrajasekar <areeganrajasekar@gmail.com>
2022-12-10 23:37:23 +0530

    Change

--- admin/collection-report.php
+++ admin/collection-report.php
@@ -32,12 +32,17 @@
                         <div class="card">
                             <div class="card-body">
                                 <h4 class="card-title"><i class="mdi me-2 mdi-chart-areaspline"></i>Generate Report</h4>
-                                <select class="form-select" aria-label="Default select example">
-  <option selected>Open this select menu</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</select>
+                                <form action="" class="row" method="post">
+                                    <div class="col-6">
+
+                                        <select class="form-select " aria-label="Default select example">
+                                            <option selected>Open this select menu</option>
+                                            <option value="1">One</option>
+                                            <option value="2">Two</option>
+                                            <option value="3">Three</option>
+                                        </select>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
